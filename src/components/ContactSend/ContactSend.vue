2e7582aea38ec453e0dbab9a32f708d25acaeb8e
klips-4 <klips-4@yandex.ru>
2022-11-28 21:34:35 +0300

    28.11.22

---
+++ src/components/ContactSend/ContactSend.vue
@@ -0,0 +1,26 @@
+<template src="./template.html"/>
+
+<script>
+
+export default {
+  name: "V-Contacts",
+  props: ["data"],
+  data() {
+    return {
+      telephone: ''
+    }
+  },
+  methods: {
+    removeContact(id) {
+      this.$emit('deleted', id)
+      this.CloseForm()
+    },
+    CloseForm() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style scoped src="./style.less" lang="less">
+</style>
